65936c7c9b57c087642f0bb0733a1df3745fe559
Igor Gerbylev <gerbylev@gmail.com>
2022-12-24 21:26:34 +0300

    Jenkinsfile

--- generate-api-client.sh
+++ generate-api-client.sh
@@ -7,7 +7,7 @@ cd $ROOT
 
 sudo rm -Rf ./openapi-generator-output
 docker run --rm -v ${PWD}:/app openapitools/openapi-generator-cli generate  \
-    -i /app/specs/mpl-rest-api.yml  -g python   -o /app/openapi-generator-output \
+    -i /app/mpl-specs/mpl-rest-api.yml  -g python   -o /app/openapi-generator-output \
     --additional-properties=packageName=mpl_api
 
 sudo chown igor:igor -R openapi-generator-output
--- generate-protobuf.sh
+++ generate-protobuf.sh
@@ -14,7 +14,7 @@ docker build . -f Dockerfile-grpc-tools \
 rm -Rf ./mpl_sdk/grpc/mpl*
 
 docker run -v $(pwd):/app grpc-tools \
- python3 -m grpc_tools.protoc -I ./specs/ --python_out=./mpl_sdk/grpc --grpc_python_out=./mpl_sdk/grpc ./specs/mpl-grpc.proto
+ python3 -m grpc_tools.protoc -I ./mpl-specs/ --python_out=./mpl_sdk/grpc --grpc_python_out=./mpl_sdk/grpc ./mpl-specs/mpl-grpc.proto
 
 #<!--  from from mpl_sdk.grpc import gate_pb2 as gate__pb2  -->
 sed -i "s/import mpl_grpc_pb2 as mpl__grpc__pb2/import mpl_sdk.grpc.mpl_grpc_pb2 as mpl__grpc__pb2/g" "mpl_sdk/grpc/mpl_grpc_pb2_grpc.py"
