6d2a3df15f74c8dbf91cf0115ef0ca671429b40e
Igor Gerbylev <gerbylev@gmail.com>
2022-12-24 21:34:16 +0300

    Jenkinsfile

--- generate-api-client.sh
+++ generate-api-client.sh
@@ -10,7 +10,7 @@ docker run --rm -v ${PWD}:/app openapitools/openapi-generator-cli generate  \
     -i /app/mpl-specs/mpl-rest-api.yml  -g python   -o /app/openapi-generator-output \
     --additional-properties=packageName=mpl_api
 
-sudo chown igor:igor -R openapi-generator-output
+sudo chown $USER:$USER -R openapi-generator-output
 rm -Rf mpl_api
 mv openapi-generator-output/mpl_api .
 rm -Rf openapi-generator-output
