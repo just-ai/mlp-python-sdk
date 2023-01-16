41ee8d02aee65a45a79fe9be03d95c670c4faac2
i.markov.user <i.markov@just-ai.com>
2023-01-16 18:08:41 +0300

    added temp log

--- mlp_sdk/transport/MlpServiceSDK.py
+++ mlp_sdk/transport/MlpServiceSDK.py
@@ -561,6 +561,8 @@ class MlpServiceSDK:
 
     @staticmethod
     def __convert_to_proto(data, payload_type, is_json):
+        print("__convert_to_proto")
+        print(data)
         if hasattr(data, 'DESCRIPTOR') and data.DESCRIPTOR.name == 'PayloadProto':
             return data
 
