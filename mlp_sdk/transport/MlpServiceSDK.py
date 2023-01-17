d6a89df274d080247bb310615bf389c6ac43d2c9
i.markov.user <i.markov@just-ai.com>
2023-01-17 12:52:27 +0300

    Revert "added temp log"
    This reverts commit c41069370d8555340eb0cb4b5e28c1128c2e8a29.

--- mlp_sdk/transport/MlpServiceSDK.py
+++ mlp_sdk/transport/MlpServiceSDK.py
@@ -418,8 +418,8 @@ class MlpServiceSDK:
             raise NotImplementedError('Predict requests are not supported by this action')
 
         prediction = self.impl.predict(data, config)
-        self.log.info("Prediction")
-        self.log.info(prediction)
+        print("Prediction")
+        print(prediction)
         response = self.__convert_to_proto(prediction, desc.output.type, is_json)
         return mlp_grpc_pb2.PredictResponseProto(data=response)
 
