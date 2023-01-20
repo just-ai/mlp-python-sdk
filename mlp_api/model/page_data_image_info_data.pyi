8f4bd695de72ffb8d259d6089c4605061da280ed
Jenkins <jenkins@just-ai.com>
2023-01-20 18:20:28 +0300

    Automatic update API spec from CI

--- mlp_api/model/page_data_image_info_data.pyi
+++ mlp_api/model/page_data_image_info_data.pyi
@@ -42,13 +42,13 @@ class PageDataImageInfoData(
             @staticmethod
             def sort() -> typing.Type['Sort']:
                 return Sort
-            first = schemas.BoolSchema
-            last = schemas.BoolSchema
             numberOfElements = schemas.Int32Schema
         
             @staticmethod
             def pageable() -> typing.Type['PageableObject']:
                 return PageableObject
+            first = schemas.BoolSchema
+            last = schemas.BoolSchema
             size = schemas.Int32Schema
             
             
@@ -82,10 +82,10 @@ class PageDataImageInfoData(
                 "totalElements": totalElements,
                 "totalPages": totalPages,
                 "sort": sort,
-                "first": first,
-                "last": last,
                 "numberOfElements": numberOfElements,
                 "pageable": pageable,
+                "first": first,
+                "last": last,
                 "size": size,
                 "content": content,
                 "number": number,
@@ -102,16 +102,16 @@ class PageDataImageInfoData(
     def __getitem__(self, name: typing_extensions.Literal["sort"]) -> 'Sort': ...
     
     @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["first"]) -> MetaOapg.properties.first: ...
-    
-    @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["last"]) -> MetaOapg.properties.last: ...
-    
-    @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["numberOfElements"]) -> MetaOapg.properties.numberOfElements: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["pageable"]) -> 'PageableObject': ...
+    
+    @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["first"]) -> MetaOapg.properties.first: ...
+    
+    @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["last"]) -> MetaOapg.properties.last: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["size"]) -> MetaOapg.properties.size: ...
@@ -128,7 +128,7 @@ class PageDataImageInfoData(
     @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "sort", "first", "last", "numberOfElements", "pageable", "size", "content", "number", "empty", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "sort", "numberOfElements", "pageable", "first", "last", "size", "content", "number", "empty", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -143,16 +143,16 @@ class PageDataImageInfoData(
     def get_item_oapg(self, name: typing_extensions.Literal["sort"]) -> typing.Union['Sort', schemas.Unset]: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["first"]) -> typing.Union[MetaOapg.properties.first, schemas.Unset]: ...
-    
-    @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["last"]) -> typing.Union[MetaOapg.properties.last, schemas.Unset]: ...
-    
-    @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["numberOfElements"]) -> typing.Union[MetaOapg.properties.numberOfElements, schemas.Unset]: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["pageable"]) -> typing.Union['PageableObject', schemas.Unset]: ...
+    
+    @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["first"]) -> typing.Union[MetaOapg.properties.first, schemas.Unset]: ...
+    
+    @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["last"]) -> typing.Union[MetaOapg.properties.last, schemas.Unset]: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["size"]) -> typing.Union[MetaOapg.properties.size, schemas.Unset]: ...
@@ -169,7 +169,7 @@ class PageDataImageInfoData(
     @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "sort", "first", "last", "numberOfElements", "pageable", "size", "content", "number", "empty", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "sort", "numberOfElements", "pageable", "first", "last", "size", "content", "number", "empty", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -179,10 +179,10 @@ class PageDataImageInfoData(
         totalElements: typing.Union[MetaOapg.properties.totalElements, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         totalPages: typing.Union[MetaOapg.properties.totalPages, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         sort: typing.Union['Sort', schemas.Unset] = schemas.unset,
-        first: typing.Union[MetaOapg.properties.first, bool, schemas.Unset] = schemas.unset,
-        last: typing.Union[MetaOapg.properties.last, bool, schemas.Unset] = schemas.unset,
         numberOfElements: typing.Union[MetaOapg.properties.numberOfElements, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         pageable: typing.Union['PageableObject', schemas.Unset] = schemas.unset,
+        first: typing.Union[MetaOapg.properties.first, bool, schemas.Unset] = schemas.unset,
+        last: typing.Union[MetaOapg.properties.last, bool, schemas.Unset] = schemas.unset,
         size: typing.Union[MetaOapg.properties.size, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         content: typing.Union[MetaOapg.properties.content, list, tuple, schemas.Unset] = schemas.unset,
         number: typing.Union[MetaOapg.properties.number, decimal.Decimal, int, schemas.Unset] = schemas.unset,
@@ -196,10 +196,10 @@ class PageDataImageInfoData(
             totalElements=totalElements,
             totalPages=totalPages,
             sort=sort,
-            first=first,
-            last=last,
             numberOfElements=numberOfElements,
             pageable=pageable,
+            first=first,
+            last=last,
             size=size,
             content=content,
             number=number,
