edfb15f1cb5b355c06031536e69d06859b21e605
Igor Gerbylev <gerbylev@gmail.com>
2023-05-18 16:31:34 +0300

    fix MDC key names

--- mlp_sdk/log/graylog_handler.py
+++ mlp_sdk/log/graylog_handler.py
@@ -8,8 +8,8 @@ class GrayLogHandler(GELFUDPHandler):
     def _make_gelf_dict(self, record):
         data_dict = super()._make_gelf_dict(record)
 
-        data_dict['graylog_env'] = os.environ.get('GRAYLOG_ENV')
-        data_dict['mlp_account_id'] = os.environ.get('MLP_ACCOUNT_ID')
-        data_dict['mlp_model_id'] = os.environ.get('MLP_MODEL_ID')
+        data_dict['env'] = os.environ.get('GRAYLOG_ENV')
+        data_dict['accountId'] = os.environ.get('MLP_ACCOUNT_ID')
+        data_dict['app'] = os.environ.get('MLP_MODEL_NAME')
 
         return data_dict
