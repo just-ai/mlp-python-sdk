13ab3ee6d1665caea6a10a61e9b212a4ef66e1fe
Protasov Vitalii <v.protasov@just-ai.com>
2023-08-28 18:36:06 +0300

    CAILA-2032: override sdk config

--- mlp_sdk/hosting/host.py
+++ mlp_sdk/hosting/host.py
@@ -1,12 +1,12 @@
+from typing import Dict
 from typing import Type
 
 import uvicorn
 from pydantic import BaseModel
 
-from mlp_sdk.transport.MlpServiceSDK import MlpServiceSDK
-
 from mlp_sdk.abstract import TASK_TYPE
 from mlp_sdk.hosting.application import prepare_app
+from mlp_sdk.transport.MlpServiceSDK import MlpServiceSDK, CONFIG
 
 
 def host(
@@ -21,21 +21,7 @@ def host(
 def host_mlp_cloud(
         task: Type[TASK_TYPE],
         params: BaseModel,
-):
-    if isinstance(params, str):
-        init_config_type = task.get_init_config_schema()
-        params = init_config_type.parse_raw(params)
-
-    mpl = MlpServiceSDK()
-    mpl.register_impl(task(params, mpl))
-    mpl.start()
-    mpl.block_until_shutdown()
-
-
-def host_mlp_cloud_with_config(
-        task: Type[TASK_TYPE],
-        params: BaseModel,
-        config
+        config: Dict = CONFIG
 ):
     if isinstance(params, str):
         init_config_type = task.get_init_config_schema()
