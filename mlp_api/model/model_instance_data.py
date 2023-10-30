1dabac4846179d3e67a5fc0d3f47502bb3b3e7de
Jenkins <jenkins@just-ai.com>
2023-10-30 15:06:44 +0300

    Automatic update API spec from CI

--- mlp_api/model/model_instance_data.py
+++ mlp_api/model/model_instance_data.py
@@ -60,6 +60,7 @@ class ModelInstanceData(
                     enum_value_to_name = {
                         "deployment": "DEPLOYMENT",
                         "pod": "POD",
+                        "docker": "DOCKER",
                         "external": "EXTERNAL",
                         "hostingServer": "HOSTING_SERVER",
                     }
@@ -71,6 +72,10 @@ class ModelInstanceData(
                 @schemas.classproperty
                 def POD(cls):
                     return cls("pod")
+                
+                @schemas.classproperty
+                def DOCKER(cls):
+                    return cls("docker")
                 
                 @schemas.classproperty
                 def EXTERNAL(cls):
