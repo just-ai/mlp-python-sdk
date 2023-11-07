b66c2aabb90cbdc86cca997b48b8c55a0d599120
Igor Gerbylev <igor.gerbylev@just-ai.com>
2023-11-07 23:08:23 +0300

    configurable logging

--- mlp_sdk/log/setup_logging.py
+++ mlp_sdk/log/setup_logging.py
@@ -4,8 +4,9 @@ import os
 from mlp_sdk.log.graylog_handler import GrayLogHandler
 
 
-def get_logger(name: str, level: str = 'INFO') -> logging.Logger:
+def get_logger(name: str) -> logging.Logger:
 
+    level = os.environ.get('MLP_LOG_LEVEL')
     logging_level = logging.getLevelName(level)
     logger = logging.getLogger(name)
     logger.setLevel(logging_level)
@@ -17,21 +18,22 @@ def get_logger(name: str, level: str = 'INFO') -> logging.Logger:
     if len(logger.handlers) != 0:
         return logger
 
-    # # create console handler
-    # if os.environ.get('MLP_CONSOLE_LOG'):
-    #     ch = logging.StreamHandler()
-    #     ch.setLevel(logging_level)
-    #
-    #     # create formatter and add it to the handlers
-    #     formatter = logging.Formatter(
-    #         "%(asctime)s - [%(levelname)s] - [%(pathname)s: %(module)s.%(funcName)s:%(lineno)d]: %(message)s")
-    #     ch.setFormatter(formatter)
-    #
-    #     logger.addHandler(ch)
-    #
-    # if os.environ.get('MLP_GRAYLOG_SERVER') and os.environ.get('MLP_GRAYLOG_PORT'):
-    #     graylog_handler = GrayLogHandler(os.environ.get('MLP_GRAYLOG_SERVER'),
-    #                                      int(os.environ.get('MLP_GRAYLOG_PORT')), extra_fields=True)
-    #     logger.addHandler(graylog_handler)
+    # create console handler
+    if not bool(os.environ.get('MLP_LOG_NO_CONSOLE', 'false')):
+        ch = logging.StreamHandler()
+        ch.setLevel(logging_level)
+
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter(
+            "%(asctime)s - [%(levelname)s] - [%(pathname)s: %(module)s.%(funcName)s:%(lineno)d]: %(message)s")
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+
+    if not bool(os.environ.get('MLP_LOG_NO_GELF', 'false')):
+        if os.environ.get('MLP_GRAYLOG_SERVER') and os.environ.get('MLP_GRAYLOG_PORT'):
+            graylog_handler = GrayLogHandler(os.environ.get('MLP_GRAYLOG_SERVER'),
+                                             int(os.environ.get('MLP_GRAYLOG_PORT')), extra_fields=True)
+            logger.addHandler(graylog_handler)
 
     return logger
