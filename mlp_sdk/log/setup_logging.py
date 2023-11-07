f380ef034a18ffe6b1f65149717b831fe3a797b4
Igor Gerbylev <igor.gerbylev@just-ai.com>
2023-11-07 15:25:41 +0300

    disable all logs temporarily

--- mlp_sdk/log/setup_logging.py
+++ mlp_sdk/log/setup_logging.py
@@ -17,21 +17,21 @@ def get_logger(name: str, level: str = 'INFO') -> logging.Logger:
     if len(logger.handlers) != 0:
         return logger
 
-    # create console handler
-    if os.environ.get('MLP_CONSOLE_LOG'):
-        ch = logging.StreamHandler()
-        ch.setLevel(logging_level)
-
-        # create formatter and add it to the handlers
-        formatter = logging.Formatter(
-            "%(asctime)s - [%(levelname)s] - [%(pathname)s: %(module)s.%(funcName)s:%(lineno)d]: %(message)s")
-        ch.setFormatter(formatter)
-
-        logger.addHandler(ch)
-
-    if os.environ.get('MLP_GRAYLOG_SERVER') and os.environ.get('MLP_GRAYLOG_PORT'):
-        graylog_handler = GrayLogHandler(os.environ.get('MLP_GRAYLOG_SERVER'),
-                                         int(os.environ.get('MLP_GRAYLOG_PORT')), extra_fields=True)
-        logger.addHandler(graylog_handler)
+    # # create console handler
+    # if os.environ.get('MLP_CONSOLE_LOG'):
+    #     ch = logging.StreamHandler()
+    #     ch.setLevel(logging_level)
+    #
+    #     # create formatter and add it to the handlers
+    #     formatter = logging.Formatter(
+    #         "%(asctime)s - [%(levelname)s] - [%(pathname)s: %(module)s.%(funcName)s:%(lineno)d]: %(message)s")
+    #     ch.setFormatter(formatter)
+    #
+    #     logger.addHandler(ch)
+    #
+    # if os.environ.get('MLP_GRAYLOG_SERVER') and os.environ.get('MLP_GRAYLOG_PORT'):
+    #     graylog_handler = GrayLogHandler(os.environ.get('MLP_GRAYLOG_SERVER'),
+    #                                      int(os.environ.get('MLP_GRAYLOG_PORT')), extra_fields=True)
+    #     logger.addHandler(graylog_handler)
 
     return logger
