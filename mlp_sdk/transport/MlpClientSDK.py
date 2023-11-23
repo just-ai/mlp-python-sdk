0521e31cd25ffaa47f3406b949909b64b90bcd76
i.markov <i.markov@just-ai.com>
2023-11-23 16:44:36 +0300

    CAILA-2348: billing token in client sdk

--- mlp_sdk/transport/MlpClientSDK.py
+++ mlp_sdk/transport/MlpClientSDK.py
@@ -27,6 +27,7 @@ class MlpClientSDK:
         self.model_id = os.environ.get('MLP_MODEL_ID')
         self.urls = None
         self.token = None
+        self.billing_token = None
         self.grpc_secure = None
         self.log = get_logger('MlpClientSDK', self.config["logging"]["level"])
         self.channel = None
@@ -34,6 +35,7 @@ class MlpClientSDK:
     def init(self, urls: Optional[List[str]] = None, token=None, grpc_secure: Optional[bool] = None):
         self.urls: List[str] = os.environ.get('MLP_GRPC_HOST', 'gate.caila.io').split(",") if not urls else urls
         self.token = os.environ['MLP_CLIENT_TOKEN'] if not token else token
+        self.billing_token = os.environ.get('MLP_BILLING_TOKEN', None)
         self.grpc_secure = os.environ.get('MLP_GRPC_SECURE', 'true').lower() == 'true' if not grpc_secure else grpc_secure
         self.log.debug("Starting mpl client for url " + self.urls[0])
 
@@ -70,6 +72,9 @@ class MlpClientSDK:
             if billing_key is not None:
                 request.headers["MLP-BILLING-KEY"] = billing_key
 
+        if self.billing_token is not None and "MLP-BILLING-KEY" not in request.headers:
+            request.headers["MLP-BILLING-KEY"] = self.billing_token
+
         response: Optional[mlp_grpc_pb2.ClientResponseProto] = self.__process_request_with_retry(request)
 
         res = response.WhichOneof('body')
@@ -99,6 +104,9 @@ class MlpClientSDK:
         return self.ext_raw(request)
 
     def ext_raw(self, request: mlp_grpc_pb2.ClientRequestProto) -> mlp_grpc_pb2.ExtendedResponseProto:
+        if self.billing_token is not None and "MLP-BILLING-KEY" not in request.headers:
+            request.headers["MLP-BILLING-KEY"] = self.billing_token
+
         response: Optional[mlp_grpc_pb2.ClientResponseProto] = self.__process_request_with_retry(request)
 
         res = response.WhichOneof('body')
