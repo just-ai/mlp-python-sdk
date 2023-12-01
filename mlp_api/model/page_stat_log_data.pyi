308c7b076331fdb2eb3c60a978a9563d49284d39
Jenkins <nx_jenkins@just-ai.com>
2023-12-01 13:59:06 +0300

    Automatic update API spec from CI

--- mlp_api/model/page_stat_log_data.pyi
+++ mlp_api/model/page_stat_log_data.pyi
@@ -38,6 +38,7 @@ class PageStatLogData(
         class properties:
             totalElements = schemas.Int64Schema
             totalPages = schemas.Int32Schema
+            first = schemas.BoolSchema
             size = schemas.Int32Schema
             
             
@@ -70,7 +71,6 @@ class PageStatLogData(
             @staticmethod
             def sort() -> typing.Type['Sort']:
                 return Sort
-            first = schemas.BoolSchema
             numberOfElements = schemas.Int32Schema
         
             @staticmethod
@@ -81,11 +81,11 @@ class PageStatLogData(
             __annotations__ = {
                 "totalElements": totalElements,
                 "totalPages": totalPages,
+                "first": first,
                 "size": size,
                 "content": content,
                 "number": number,
                 "sort": sort,
-                "first": first,
                 "numberOfElements": numberOfElements,
                 "pageable": pageable,
                 "last": last,
@@ -99,6 +99,9 @@ class PageStatLogData(
     def __getitem__(self, name: typing_extensions.Literal["totalPages"]) -> MetaOapg.properties.totalPages: ...
     
     @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["first"]) -> MetaOapg.properties.first: ...
+    
+    @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["size"]) -> MetaOapg.properties.size: ...
     
     @typing.overload
@@ -109,9 +112,6 @@ class PageStatLogData(
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["sort"]) -> 'Sort': ...
-    
-    @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["first"]) -> MetaOapg.properties.first: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["numberOfElements"]) -> MetaOapg.properties.numberOfElements: ...
@@ -128,7 +128,7 @@ class PageStatLogData(
     @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "size", "content", "number", "sort", "first", "numberOfElements", "pageable", "last", "empty", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "first", "size", "content", "number", "sort", "numberOfElements", "pageable", "last", "empty", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -138,6 +138,9 @@ class PageStatLogData(
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["totalPages"]) -> typing.Union[MetaOapg.properties.totalPages, schemas.Unset]: ...
+    
+    @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["first"]) -> typing.Union[MetaOapg.properties.first, schemas.Unset]: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["size"]) -> typing.Union[MetaOapg.properties.size, schemas.Unset]: ...
@@ -150,9 +153,6 @@ class PageStatLogData(
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["sort"]) -> typing.Union['Sort', schemas.Unset]: ...
-    
-    @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["first"]) -> typing.Union[MetaOapg.properties.first, schemas.Unset]: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["numberOfElements"]) -> typing.Union[MetaOapg.properties.numberOfElements, schemas.Unset]: ...
@@ -169,7 +169,7 @@ class PageStatLogData(
     @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "size", "content", "number", "sort", "first", "numberOfElements", "pageable", "last", "empty", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["totalElements", "totalPages", "first", "size", "content", "number", "sort", "numberOfElements", "pageable", "last", "empty", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -178,11 +178,11 @@ class PageStatLogData(
         *_args: typing.Union[dict, frozendict.frozendict, ],
         totalElements: typing.Union[MetaOapg.properties.totalElements, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         totalPages: typing.Union[MetaOapg.properties.totalPages, decimal.Decimal, int, schemas.Unset] = schemas.unset,
+        first: typing.Union[MetaOapg.properties.first, bool, schemas.Unset] = schemas.unset,
         size: typing.Union[MetaOapg.properties.size, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         content: typing.Union[MetaOapg.properties.content, list, tuple, schemas.Unset] = schemas.unset,
         number: typing.Union[MetaOapg.properties.number, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         sort: typing.Union['Sort', schemas.Unset] = schemas.unset,
-        first: typing.Union[MetaOapg.properties.first, bool, schemas.Unset] = schemas.unset,
         numberOfElements: typing.Union[MetaOapg.properties.numberOfElements, decimal.Decimal, int, schemas.Unset] = schemas.unset,
         pageable: typing.Union['PageableObject', schemas.Unset] = schemas.unset,
         last: typing.Union[MetaOapg.properties.last, bool, schemas.Unset] = schemas.unset,
@@ -195,11 +195,11 @@ class PageStatLogData(
             *_args,
             totalElements=totalElements,
             totalPages=totalPages,
+            first=first,
             size=size,
             content=content,
             number=number,
             sort=sort,
-            first=first,
             numberOfElements=numberOfElements,
             pageable=pageable,
             last=last,
