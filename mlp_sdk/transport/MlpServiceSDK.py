7e1853d89ceca237507ba480b210810a5816a894
i.markov <i.markov@just-ai.com>
2023-12-07 19:40:55 +0300

    test

--- mlp_sdk/transport/MlpServiceSDK.py
+++ mlp_sdk/transport/MlpServiceSDK.py
@@ -519,9 +519,9 @@ class MlpServiceSDK:
         responses_protos = []
 
         for index, result in enumerate(res_list):
-            print(f"Batch result: {result.__dict__}")
+            self.log.info(f"Batch result: {result.__dict__}")
             converted = self.__convert_to_proto(result, desc.output.type, is_json)
-            print(f"Batch result converted: {converted.__dict__}")
+            self.log.info(f"Batch result converted: {converted}")
             request_id = request.data[index].requestId
             proto = mlp_grpc_pb2.BatchPayloadResponseProto(
                 requestId=request_id,
