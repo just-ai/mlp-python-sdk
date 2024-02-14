126db2c045a484d6942921358b975b53469a71cc
Jenkins <nx_jenkins@just-ai.com>
2024-02-14 18:45:20 +0300

    Automatic update API spec from CI

--- mlp_api/model/resource_group_data.py
+++ mlp_api/model/resource_group_data.py
@@ -97,6 +97,10 @@ class ResourceGroupData(
                 @schemas.classproperty
                 def PUBLIC(cls):
                     return cls("PUBLIC")
+        
+            @staticmethod
+            def autoScalingConfiguration() -> typing.Type['ResourceGroupAutoScalingConfiguration']:
+                return ResourceGroupAutoScalingConfiguration
             __annotations__ = {
                 "name": name,
                 "tolerations": tolerations,
@@ -109,6 +113,7 @@ class ResourceGroupData(
                 "enabledEviction": enabledEviction,
                 "resourceGroupType": resourceGroupType,
                 "access": access,
+                "autoScalingConfiguration": autoScalingConfiguration,
             }
     
     tolerations: MetaOapg.properties.tolerations
@@ -149,9 +154,12 @@ class ResourceGroupData(
     def __getitem__(self, name: typing_extensions.Literal["access"]) -> MetaOapg.properties.access: ...
     
     @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["autoScalingConfiguration"]) -> 'ResourceGroupAutoScalingConfiguration': ...
+    
+    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", "autoScalingConfiguration", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -190,9 +198,12 @@ class ResourceGroupData(
     def get_item_oapg(self, name: typing_extensions.Literal["access"]) -> typing.Union[MetaOapg.properties.access, schemas.Unset]: ...
     
     @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["autoScalingConfiguration"]) -> typing.Union['ResourceGroupAutoScalingConfiguration', schemas.Unset]: ...
+    
+    @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", "autoScalingConfiguration", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -210,6 +221,7 @@ class ResourceGroupData(
         enabledEviction: typing.Union[MetaOapg.properties.enabledEviction, bool, schemas.Unset] = schemas.unset,
         resourceGroupType: typing.Union[MetaOapg.properties.resourceGroupType, str, schemas.Unset] = schemas.unset,
         access: typing.Union[MetaOapg.properties.access, str, schemas.Unset] = schemas.unset,
+        autoScalingConfiguration: typing.Union['ResourceGroupAutoScalingConfiguration', schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'ResourceGroupData':
@@ -227,6 +239,9 @@ class ResourceGroupData(
             enabledEviction=enabledEviction,
             resourceGroupType=resourceGroupType,
             access=access,
+            autoScalingConfiguration=autoScalingConfiguration,
             _configuration=_configuration,
             **kwargs,
         )
+
+from mlp_api.model.resource_group_auto_scaling_configuration import ResourceGroupAutoScalingConfiguration
--- mlp_api/model/resource_group_data.pyi
+++ mlp_api/model/resource_group_data.pyi
@@ -82,6 +82,10 @@ class ResourceGroupData(
                 @schemas.classproperty
                 def PUBLIC(cls):
                     return cls("PUBLIC")
+        
+            @staticmethod
+            def autoScalingConfiguration() -> typing.Type['ResourceGroupAutoScalingConfiguration']:
+                return ResourceGroupAutoScalingConfiguration
             __annotations__ = {
                 "name": name,
                 "tolerations": tolerations,
@@ -94,6 +98,7 @@ class ResourceGroupData(
                 "enabledEviction": enabledEviction,
                 "resourceGroupType": resourceGroupType,
                 "access": access,
+                "autoScalingConfiguration": autoScalingConfiguration,
             }
     
     tolerations: MetaOapg.properties.tolerations
@@ -134,9 +139,12 @@ class ResourceGroupData(
     def __getitem__(self, name: typing_extensions.Literal["access"]) -> MetaOapg.properties.access: ...
     
     @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["autoScalingConfiguration"]) -> 'ResourceGroupAutoScalingConfiguration': ...
+    
+    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", "autoScalingConfiguration", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -175,9 +183,12 @@ class ResourceGroupData(
     def get_item_oapg(self, name: typing_extensions.Literal["access"]) -> typing.Union[MetaOapg.properties.access, schemas.Unset]: ...
     
     @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["autoScalingConfiguration"]) -> typing.Union['ResourceGroupAutoScalingConfiguration', schemas.Unset]: ...
+    
+    @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["name", "tolerations", "affinity", "accountId", "accountName", "ownerId", "isDefault", "enabledAutoScaling", "enabledEviction", "resourceGroupType", "access", "autoScalingConfiguration", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -195,6 +206,7 @@ class ResourceGroupData(
         enabledEviction: typing.Union[MetaOapg.properties.enabledEviction, bool, schemas.Unset] = schemas.unset,
         resourceGroupType: typing.Union[MetaOapg.properties.resourceGroupType, str, schemas.Unset] = schemas.unset,
         access: typing.Union[MetaOapg.properties.access, str, schemas.Unset] = schemas.unset,
+        autoScalingConfiguration: typing.Union['ResourceGroupAutoScalingConfiguration', schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'ResourceGroupData':
@@ -212,6 +224,9 @@ class ResourceGroupData(
             enabledEviction=enabledEviction,
             resourceGroupType=resourceGroupType,
             access=access,
+            autoScalingConfiguration=autoScalingConfiguration,
             _configuration=_configuration,
             **kwargs,
         )
+
+from mlp_api.model.resource_group_auto_scaling_configuration import ResourceGroupAutoScalingConfiguration
