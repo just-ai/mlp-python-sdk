b8b5f949125e7c8085fb925b39f6cea6685dd0b7
Jenkins <nx_jenkins@just-ai.com>
2024-03-07 19:45:13 +0300

    Automatic update API spec from CI

--- mlp_api/apis/paths/api_mlpgate_account_account_application_application_set_cookie.py
+++ mlp_api/apis/paths/api_mlpgate_account_account_application_application_set_cookie.py
@@ -1,7 +1,9 @@
 from mlp_api.paths.api_mlpgate_account_account_application_application_set_cookie.get import ApiForget
+from mlp_api.paths.api_mlpgate_account_account_application_application_set_cookie.options import ApiForoptions
 
 
 class ApiMlpgateAccountAccountApplicationApplicationSetCookie(
     ApiForget,
+    ApiForoptions,
 ):
     pass
