3a4cab9de93863d33a4b355ae74495c695018422
Jenkins <nx_jenkins@just-ai.com>
2024-05-22 15:56:18 +0300

    Automatic update API spec from CI

--- mlp_api/model/difference_i_account_data_dump.pyi
+++ mlp_api/model/difference_i_account_data_dump.pyi
@@ -38,57 +38,57 @@ class DifferenceIAccountDataDump(
         class properties:
         
             @staticmethod
-            def before() -> typing.Type['AccountDataDump']:
+            def after() -> typing.Type['AccountDataDump']:
                 return AccountDataDump
         
             @staticmethod
-            def after() -> typing.Type['AccountDataDump']:
+            def before() -> typing.Type['AccountDataDump']:
                 return AccountDataDump
             __annotations__ = {
-                "before": before,
                 "after": after,
+                "before": before,
             }
-    
-    @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["before"]) -> 'AccountDataDump': ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["after"]) -> 'AccountDataDump': ...
     
     @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["before"]) -> 'AccountDataDump': ...
+    
+    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["before", "after", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["after", "before", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["before"]) -> typing.Union['AccountDataDump', schemas.Unset]: ...
+    def get_item_oapg(self, name: typing_extensions.Literal["after"]) -> typing.Union['AccountDataDump', schemas.Unset]: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["after"]) -> typing.Union['AccountDataDump', schemas.Unset]: ...
+    def get_item_oapg(self, name: typing_extensions.Literal["before"]) -> typing.Union['AccountDataDump', schemas.Unset]: ...
     
     @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["before", "after", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["after", "before", ], str]):
         return super().get_item_oapg(name)
     
 
     def __new__(
         cls,
         *_args: typing.Union[dict, frozendict.frozendict, ],
-        before: typing.Union['AccountDataDump', schemas.Unset] = schemas.unset,
         after: typing.Union['AccountDataDump', schemas.Unset] = schemas.unset,
+        before: typing.Union['AccountDataDump', schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'DifferenceIAccountDataDump':
         return super().__new__(
             cls,
             *_args,
-            before=before,
             after=after,
+            before=before,
             _configuration=_configuration,
             **kwargs,
         )
