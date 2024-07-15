18aee3359a68b9966716d1a81a967ae7062c5750
Anastasiia Ikonnikova <a.ikonnikova@just-ai.com>
2024-07-15 19:54:24 +0500

    test: example

---
+++ examples/request_to_llm.py
@@ -0,0 +1,26 @@
+import ast
+from pprint import pprint
+
+from mlp_api.api.process_endpoint_api import ProcessEndpointApi
+from mlp_api.models.chat_role import ChatRole
+from mlp_api.models.predict_request_data import PredictRequestData
+from mlp_sdk.transport.MlpClientSDK import MlpRestClient
+
+if __name__ == "__main__":
+    mlp_api_key = ""
+    account_id = "just-ai"
+    model_name = "openai-proxy"
+    mlp_rest_client = MlpRestClient(url="https://caila.io", token=mlp_api_key)
+    mlp_prediction_client = ProcessEndpointApi(mlp_rest_client)
+
+    request = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": ChatRole.USER, "content": "hello"}],
+    }
+    response = mlp_prediction_client.predict_with_config(
+        account_id,
+        model_name,
+        predict_request_data=PredictRequestData(data=request),
+    )
+    result = ast.literal_eval(response)
+    pprint(result)
