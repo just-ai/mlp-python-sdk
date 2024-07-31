7cae2948628bd117aff5de09a76b5d5833cb9654
Jenkins <nx_jenkins@just-ai.com>
2024-07-31 12:28:00 +0300

    Automatic update API spec from CI

--- mlp_api/models/page_stat_log_data.py
+++ mlp_api/models/page_stat_log_data.py
@@ -30,16 +30,16 @@ class PageStatLogData(BaseModel):
     """
     total_elements: Optional[StrictInt] = Field(default=None, alias="totalElements")
     total_pages: Optional[StrictInt] = Field(default=None, alias="totalPages")
-    pageable: Optional[PageableObject] = None
     first: Optional[StrictBool] = None
+    pageable: Optional[PageableObject] = None
     size: Optional[StrictInt] = None
     content: Optional[conlist(StatLogData)] = None
     number: Optional[StrictInt] = None
     sort: Optional[SortObject] = None
-    last: Optional[StrictBool] = None
     number_of_elements: Optional[StrictInt] = Field(default=None, alias="numberOfElements")
+    last: Optional[StrictBool] = None
     empty: Optional[StrictBool] = None
-    __properties = ["totalElements", "totalPages", "pageable", "first", "size", "content", "number", "sort", "last", "numberOfElements", "empty"]
+    __properties = ["totalElements", "totalPages", "first", "pageable", "size", "content", "number", "sort", "numberOfElements", "last", "empty"]
 
     class Config:
         """Pydantic configuration"""
@@ -92,14 +92,14 @@ class PageStatLogData(BaseModel):
         _obj = PageStatLogData.parse_obj({
             "total_elements": obj.get("totalElements"),
             "total_pages": obj.get("totalPages"),
-            "pageable": PageableObject.from_dict(obj.get("pageable")) if obj.get("pageable") is not None else None,
             "first": obj.get("first"),
+            "pageable": PageableObject.from_dict(obj.get("pageable")) if obj.get("pageable") is not None else None,
             "size": obj.get("size"),
             "content": [StatLogData.from_dict(_item) for _item in obj.get("content")] if obj.get("content") is not None else None,
             "number": obj.get("number"),
             "sort": SortObject.from_dict(obj.get("sort")) if obj.get("sort") is not None else None,
-            "last": obj.get("last"),
             "number_of_elements": obj.get("numberOfElements"),
+            "last": obj.get("last"),
             "empty": obj.get("empty")
         })
         return _obj
