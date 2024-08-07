b1806381a9a9d37b13b909075c038b99fa2986a3
Jenkins <nx_jenkins@just-ai.com>
2024-08-08 01:21:34 +0300

    Automatic update API spec from CI

--- mlp_api/api/admin_endpoint_api.py
+++ mlp_api/api/admin_endpoint_api.py
@@ -30,6 +30,7 @@ from mlp_api.models.account_info_data import AccountInfoData
 from mlp_api.models.account_limits_data import AccountLimitsData
 from mlp_api.models.difference_i_account_config_dump import DifferenceIAccountConfigDump
 from mlp_api.models.difference_i_account_data_dump import DifferenceIAccountDataDump
+from mlp_api.models.empty_users_response import EmptyUsersResponse
 from mlp_api.models.resource_groups_data import ResourceGroupsData
 
 from mlp_api.api_client import ApiClient
@@ -51,6 +52,160 @@ class AdminEndpointApi:
         if api_client is None:
             api_client = ApiClient.get_default()
         self.api_client = api_client
+
+    @validate_arguments
+    def delete_empty_minio_users(self, count : StrictInt, dry_run : StrictBool, mlp_api_key : Annotated[Optional[StrictStr], Field(description="token to use instead of a session")] = None, **kwargs) -> EmptyUsersResponse:  # noqa: E501
+        """delete_empty_minio_users  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.delete_empty_minio_users(count, dry_run, mlp_api_key, async_req=True)
+        >>> result = thread.get()
+
+        :param count: (required)
+        :type count: int
+        :param dry_run: (required)
+        :type dry_run: bool
+        :param mlp_api_key: token to use instead of a session
+        :type mlp_api_key: str
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _request_timeout: timeout setting for this request.
+               If one number provided, it will be total request
+               timeout. It can also be a pair (tuple) of
+               (connection, read) timeouts.
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: EmptyUsersResponse
+        """
+        kwargs['_return_http_data_only'] = True
+        if '_preload_content' in kwargs:
+            message = "Error! Please call the delete_empty_minio_users_with_http_info method with `_preload_content` instead and obtain raw data from ApiResponse.raw_data"  # noqa: E501
+            raise ValueError(message)
+        return self.delete_empty_minio_users_with_http_info(count, dry_run, mlp_api_key, **kwargs)  # noqa: E501
+
+    @validate_arguments
+    def delete_empty_minio_users_with_http_info(self, count : StrictInt, dry_run : StrictBool, mlp_api_key : Annotated[Optional[StrictStr], Field(description="token to use instead of a session")] = None, **kwargs) -> ApiResponse:  # noqa: E501
+        """delete_empty_minio_users  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.delete_empty_minio_users_with_http_info(count, dry_run, mlp_api_key, async_req=True)
+        >>> result = thread.get()
+
+        :param count: (required)
+        :type count: int
+        :param dry_run: (required)
+        :type dry_run: bool
+        :param mlp_api_key: token to use instead of a session
+        :type mlp_api_key: str
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _preload_content: if False, the ApiResponse.data will
+                                 be set to none and raw_data will store the
+                                 HTTP response body without reading/decoding.
+                                 Default is True.
+        :type _preload_content: bool, optional
+        :param _return_http_data_only: response data instead of ApiResponse
+                                       object with status code, headers, etc
+        :type _return_http_data_only: bool, optional
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the authentication
+                              in the spec for a single request.
+        :type _request_auth: dict, optional
+        :type _content_type: string, optional: force content-type for the request
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: tuple(EmptyUsersResponse, status_code(int), headers(HTTPHeaderDict))
+        """
+
+        _params = locals()
+
+        _all_params = [
+            'count',
+            'dry_run',
+            'mlp_api_key'
+        ]
+        _all_params.extend(
+            [
+                'async_req',
+                '_return_http_data_only',
+                '_preload_content',
+                '_request_timeout',
+                '_request_auth',
+                '_content_type',
+                '_headers'
+            ]
+        )
+
+        # validate the arguments
+        for _key, _val in _params['kwargs'].items():
+            if _key not in _all_params:
+                raise ApiTypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method delete_empty_minio_users" % _key
+                )
+            _params[_key] = _val
+        del _params['kwargs']
+
+        _collection_formats = {}
+
+        # process the path parameters
+        _path_params = {}
+
+        # process the query parameters
+        _query_params = []
+        if _params.get('count') is not None:  # noqa: E501
+            _query_params.append(('count', _params['count']))
+
+        if _params.get('dry_run') is not None:  # noqa: E501
+            _query_params.append(('dryRun', _params['dry_run']))
+
+        # process the header parameters
+        _header_params = dict(_params.get('_headers', {}))
+        if _params['mlp_api_key'] is not None:
+            _header_params['MLP-API-KEY'] = _params['mlp_api_key']
+
+        # process the form parameters
+        _form_params = []
+        _files = {}
+        # process the body parameter
+        _body_params = None
+        # set the HTTP header `Accept`
+        _header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # authentication setting
+        _auth_settings = []  # noqa: E501
+
+        _response_types_map = {
+            '200': "EmptyUsersResponse",
+        }
+
+        return self.api_client.call_api(
+            '/api/mlpgate/admin/minio/delete-empty-users', 'POST',
+            _path_params,
+            _query_params,
+            _header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            response_types_map=_response_types_map,
+            auth_settings=_auth_settings,
+            async_req=_params.get('async_req'),
+            _return_http_data_only=_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=_params.get('_preload_content', True),
+            _request_timeout=_params.get('_request_timeout'),
+            collection_formats=_collection_formats,
+            _request_auth=_params.get('_request_auth'))
 
     @validate_arguments
     def get_account(self, account : Annotated[StrictStr, Field(..., description="Account id or account name")], mlp_api_key : Annotated[Optional[StrictStr], Field(description="token to use instead of a session")] = None, **kwargs) -> AccountInfoData:  # noqa: E501
