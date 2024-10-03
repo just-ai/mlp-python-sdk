13b5afbf4bc721240a79528e3c8d5a540943e976
Jenkins <jenkins@just-ai.com>
2024-10-03 14:01:01 +0300

    Automatic update API spec from CI

--- mlp_api/models/page_stat_log_data.py
+++ mlp_api/models/page_stat_log_data.py
@@ -35,11 +35,11 @@ class PageStatLogData(BaseModel):
     content: Optional[conlist(StatLogData)] = None
     number: Optional[StrictInt] = None
     sort: Optional[SortObject] = None
-    number_of_elements: Optional[StrictInt] = Field(default=None, alias="numberOfElements")
     pageable: Optional[PageableObject] = None
+    number_of_elements: Optional[StrictInt] = Field(default=None, alias="numberOfElements")
     last: Optional[StrictBool] = None
     empty: Optional[StrictBool] = None
-    __properties = ["totalElements", "totalPages", "first", "size", "content", "number", "sort", "numberOfElements", "pageable", "last", "empty"]
+    __properties = ["totalElements", "totalPages", "first", "size", "content", "number", "sort", "pageable", "numberOfElements", "last", "empty"]
 
     class Config:
         """Pydantic configuration"""
@@ -97,8 +97,8 @@ class PageStatLogData(BaseModel):
             "content": [StatLogData.from_dict(_item) for _item in obj.get("content")] if obj.get("content") is not None else None,
             "number": obj.get("number"),
             "sort": SortObject.from_dict(obj.get("sort")) if obj.get("sort") is not None else None,
-            "number_of_elements": obj.get("numberOfElements"),
             "pageable": PageableObject.from_dict(obj.get("pageable")) if obj.get("pageable") is not None else None,
+            "number_of_elements": obj.get("numberOfElements"),
             "last": obj.get("last"),
             "empty": obj.get("empty")
         })
