34f7703c77a1eb0701e17ae02279b458663f6d51
Jenkins <jenkins@just-ai.com>
2024-12-05 12:50:47 +0300

    Automatic update API spec from CI

--- mlp_api/models/server_template_data.py
+++ mlp_api/models/server_template_data.py
@@ -36,7 +36,8 @@ class ServerTemplateData(BaseModel):
     cost_price: Union[StrictFloat, StrictInt] = Field(default=..., alias="costPrice")
     tariffication_period: Optional[StrictStr] = Field(default=None, alias="tarifficationPeriod")
     is_available: Optional[StrictBool] = Field(default=None, alias="isAvailable")
-    __properties = ["id", "name", "type", "capacity", "description", "rawConfiguration", "tarifficationPrice", "costPrice", "tarifficationPeriod", "isAvailable"]
+    availability: Optional[StrictStr] = None
+    __properties = ["id", "name", "type", "capacity", "description", "rawConfiguration", "tarifficationPrice", "costPrice", "tarifficationPeriod", "isAvailable", "availability"]
 
     @validator('type')
     def type_validate_enum(cls, value):
@@ -53,6 +54,16 @@ class ServerTemplateData(BaseModel):
 
         if value not in ('SECOND', 'MINUTE', 'HOUR', 'DAY', 'MONTH', 'YEAR'):
             raise ValueError("must be one of enum values ('SECOND', 'MINUTE', 'HOUR', 'DAY', 'MONTH', 'YEAR')")
+        return value
+
+    @validator('availability')
+    def availability_validate_enum(cls, value):
+        """Validates the enum"""
+        if value is None:
+            return value
+
+        if value not in ('AVAILABLE', 'UNAVAILABLE', 'UNKNOWN'):
+            raise ValueError("must be one of enum values ('AVAILABLE', 'UNAVAILABLE', 'UNKNOWN')")
         return value
 
     class Config:
@@ -103,7 +114,8 @@ class ServerTemplateData(BaseModel):
             "tariffication_price": obj.get("tarifficationPrice"),
             "cost_price": obj.get("costPrice"),
             "tariffication_period": obj.get("tarifficationPeriod"),
-            "is_available": obj.get("isAvailable")
+            "is_available": obj.get("isAvailable"),
+            "availability": obj.get("availability")
         })
         return _obj
 
