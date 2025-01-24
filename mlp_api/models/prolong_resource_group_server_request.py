3c4df7741b5ca74afd7173bc21724ba22d21d209
Jenkins <jenkins@just-ai.com>
2025-01-24 20:35:13 +0300

    Automatic update API spec from CI

--- mlp_api/models/prolong_resource_group_server_request.py
+++ mlp_api/models/prolong_resource_group_server_request.py
@@ -18,17 +18,15 @@ import re  # noqa: F401
 import json
 
 
-from typing import Optional
-from pydantic import BaseModel, StrictBool
 
-class SortObject(BaseModel):
+from pydantic import BaseModel, Field, StrictInt
+
+class ProlongResourceGroupServerRequest(BaseModel):
     """
-    SortObject
+    ProlongResourceGroupServerRequest
     """
-    sorted: Optional[StrictBool] = None
-    empty: Optional[StrictBool] = None
-    unsorted: Optional[StrictBool] = None
-    __properties = ["sorted", "empty", "unsorted"]
+    additional_server_lifetime_minutes: StrictInt = Field(default=..., alias="additionalServerLifetimeMinutes")
+    __properties = ["additionalServerLifetimeMinutes"]
 
     class Config:
         """Pydantic configuration"""
@@ -44,8 +42,8 @@ class SortObject(BaseModel):
         return json.dumps(self.to_dict())
 
     @classmethod
-    def from_json(cls, json_str: str) -> SortObject:
-        """Create an instance of SortObject from a JSON string"""
+    def from_json(cls, json_str: str) -> ProlongResourceGroupServerRequest:
+        """Create an instance of ProlongResourceGroupServerRequest from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self):
@@ -57,18 +55,16 @@ class SortObject(BaseModel):
         return _dict
 
     @classmethod
-    def from_dict(cls, obj: dict) -> SortObject:
-        """Create an instance of SortObject from a dict"""
+    def from_dict(cls, obj: dict) -> ProlongResourceGroupServerRequest:
+        """Create an instance of ProlongResourceGroupServerRequest from a dict"""
         if obj is None:
             return None
 
         if not isinstance(obj, dict):
-            return SortObject.parse_obj(obj)
+            return ProlongResourceGroupServerRequest.parse_obj(obj)
 
-        _obj = SortObject.parse_obj({
-            "sorted": obj.get("sorted"),
-            "empty": obj.get("empty"),
-            "unsorted": obj.get("unsorted")
+        _obj = ProlongResourceGroupServerRequest.parse_obj({
+            "additional_server_lifetime_minutes": obj.get("additionalServerLifetimeMinutes")
         })
         return _obj
 
