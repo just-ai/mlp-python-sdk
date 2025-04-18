4a90fe1e5ac7a66bec4622ac3739cdb05415b6c1
Igor Gerbylev <igor.gerbylev@just-ai.com>
2025-04-18 15:31:46 +0300

    ff

--- build-scripts/generate-grpc.sh
+++ build-scripts/generate-grpc.sh
@@ -14,7 +14,6 @@ docker build . -f build-scripts/Dockerfile-dev \
 rm -f ./src/mlp_sdk/mlp_connector/grpc_/*.py*
 
 docker run -v $(pwd):/app mlp-python-sdk-tools \
- pip install grpcio-tools && \
- python3 -m grpc_tools.protoc -I ./api-specs --python_out=./src/mlp_sdk/mlp_connector/grpc_ --pyi_out=./src/mlp_sdk/mlp_connector/grpc_ --grpc_python_out=./src/mlp_sdk/mlp_connector/grpc_ ./api-specs/mlp-grpc.proto
+  .venv/bin/python -m grpc_tools.protoc -I ./api-specs --python_out=./src/mlp_sdk/mlp_connector/grpc_ --pyi_out=./src/mlp_sdk/mlp_connector/grpc_ --grpc_python_out=./src/mlp_sdk/mlp_connector/grpc_ ./api-specs/mlp-grpc.proto
 
 sed -i "s/import mlp_grpc_pb2 as mlp__grpc__pb2/import mlp_sdk.mlp_connector.grpc_.mlp_grpc_pb2 as mlp__grpc__pb2/g" "src/mlp_sdk/mlp_connector/grpc_/mlp_grpc_pb2_grpc.py"
