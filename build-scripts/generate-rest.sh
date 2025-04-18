79d5e3961479525e86465f9151a75fa2325cc744
Igor Gerbylev <igor.gerbylev@just-ai.com>
2025-04-18 17:37:20 +0300

    change name back to mlp-sdk

--- build-scripts/generate-rest.sh
+++ build-scripts/generate-rest.sh
@@ -15,22 +15,22 @@ cd $PROJECT_ROOT
 
 echo $(pwd)
 
-rm -Rf src/mlp_api
-rm -Rf src/storage_api
+rm -Rf src/mlp_sdk/mlp_api
+rm -Rf src/mlp_sdk/storage_api
 
 docker run --rm -v "${PWD}":/app openapi-generator generate  \
     -i /app/api-specs/mlp-rest-api.yml  -g python-pydantic-v1   -o /app/openapi-generator-output/mlp-rest-api \
-    --additional-properties=packageName=mlp_api
+    --additional-properties=packageName=mlp_sdk.mlp_api
 
 docker run --rm -v "${PWD}":/app openapi-generator generate  \
     -i /app/api-specs/mlp-storage-api.yml  -g python-pydantic-v1   -o /app/openapi-generator-output/mlp-storage-api \
-    --additional-properties=packageName=storage_api
+    --additional-properties=packageName=mlp_sdk.storage_api
 
 
 find openapi-generator-output -name "*.py" -exec grep -l "allow_population_by_field_name" {} \; | xargs sed -i 's/allow_population_by_field_name/populate_by_name/g'
 
-mv openapi-generator-output/mlp-rest-api/mlp_api src/
-mv openapi-generator-output/mlp-storage-api/storage_api src/
+mv openapi-generator-output/mlp-rest-api/mlp_sdk/mlp_api src/mlp_sdk/
+mv openapi-generator-output/mlp-storage-api/mlp_sdk/storage_api src/mlp_sdk/
 
 rm -Rf openapi-generator-output
 
