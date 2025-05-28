5ec0ffde35fa9e0d2bbb4f3c0b24f5762fd184cb
Jenkins <jenkins@just-ai.com>
2025-05-28 17:57:43 +0300

    Automatic update API spec from CI

--- mlp_api/models/create_update_server_template_data.py
+++ mlp_api/models/create_update_server_template_data.py
@@ -40,8 +40,8 @@ class CreateUpdateServerTemplateData(BaseModel):
     @validator('type')
     def type_validate_enum(cls, value):
         """Validates the enum"""
-        if value not in ('DOCKER', 'KUBERNETES', 'HOSTING_SERVER', 'SHARED_RESOURCE_QUOTA'):
-            raise ValueError("must be one of enum values ('DOCKER', 'KUBERNETES', 'HOSTING_SERVER', 'SHARED_RESOURCE_QUOTA')")
+        if value not in ('DOCKER', 'VAST_AI', 'KUBERNETES', 'HOSTING_SERVER', 'SHARED_RESOURCE_QUOTA'):
+            raise ValueError("must be one of enum values ('DOCKER', 'VAST_AI', 'KUBERNETES', 'HOSTING_SERVER', 'SHARED_RESOURCE_QUOTA')")
         return value
 
     @validator('tariffication_period')
