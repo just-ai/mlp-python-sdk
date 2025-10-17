9712fd3d3877387e839007306e5a8d252f637aa1
Anton Manankov <a.manankov@just-ai.com>
2025-10-17 10:20:06 +0000

    CI(CAILA-5044): Ошибка Permission Error

--- build-scripts/generate-rest.sh
+++ build-scripts/generate-rest.sh
@@ -5,27 +5,32 @@ ROOT=$(realpath $(dirname $0))
 cd "$ROOT"
 PROJECT_ROOT=$(realpath $ROOT/..)
 
-docker build . -f Dockerfile-openapi \
-            --build-arg USER=$USER \
-            --build-arg UID=$(id -u) \
-            --build-arg GID=$(id -g) \
-            -t openapi-generator
-
 cd $PROJECT_ROOT
 
+docker build . -f ./build-scripts/Dockerfile-openapi -t openapi-generator \
+            --build-arg USER=just \
+            --build-arg UID=1230 \
+            --build-arg GID=1230
+
 echo $(pwd)
+mkdir openapi-generator-output
 
 rm -Rf src/mlp_sdk/mlp_api
 rm -Rf src/mlp_sdk/storage_api
 
-docker run --rm -v "${PWD}":/app openapi-generator generate  \
-    -i /app/api-specs/mlp-rest-api.yml  -g python-pydantic-v1   -o /app/openapi-generator-output/mlp-rest-api \
+docker run --name openapi-container openapi-generator generate  \
+    -i api-specs/mlp-rest-api.yml  -g python-pydantic-v1   -o openapi-generator-output/mlp-rest-api \
     --additional-properties=packageName=mlp_sdk.mlp_api
 
-docker run --rm -v "${PWD}":/app openapi-generator generate  \
-    -i /app/api-specs/mlp-storage-api.yml  -g python-pydantic-v1   -o /app/openapi-generator-output/mlp-storage-api \
+docker cp openapi-container:/app/openapi-generator-output/mlp-rest-api ./openapi-generator-output/mlp-rest-api
+docker rm openapi-container
+
+docker run --name openapi-container openapi-generator generate  \
+    -i api-specs/mlp-storage-api.yml  -g python-pydantic-v1   -o openapi-generator-output/mlp-storage-api \
     --additional-properties=packageName=mlp_sdk.storage_api
 
+docker cp openapi-container:/app/openapi-generator-output/mlp-storage-api ./openapi-generator-output/mlp-storage-api
+docker rm openapi-container
 
 find openapi-generator-output -name "*.py" -exec grep -l "allow_population_by_field_name" {} \; | xargs sed -i 's/allow_population_by_field_name/populate_by_name/g'
 
@@ -35,4 +40,3 @@ mv openapi-generator-output/mlp-rest-api/mlp_sdk/mlp_api src/mlp_sdk/
 mv openapi-generator-output/mlp-storage-api/mlp_sdk/storage_api src/mlp_sdk/
 
 rm -Rf openapi-generator-output
-
