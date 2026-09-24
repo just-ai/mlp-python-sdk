a26513e671a04b3de70bfe226a6568a22224f79b
Jenkins Pipeline <nx_jenkins@just-ai.com>
2026-09-24 14:50:44 +0300

    Automatic update API spec from CI

--- mlp_api/models/provider_pricing.py
+++ mlp_api/models/provider_pricing.py
@@ -37,8 +37,8 @@ class ProviderPricing(BaseModel):
     @validator('provider')
     def provider_validate_enum(cls, value):
         """Validates the enum"""
-        if value not in ('ANTHROPIC', 'DEEPSEEK', 'FAL', 'GAMMA', 'GOOGLE', 'HEYGEN', 'JUSTAI', 'LINKHARBOR', 'MANUS', 'OPENAI', 'OPENROUTER', 'PARALLEL', 'PERPLEXITY', 'SBER', 'VEAI', 'YANDEX', 'OTHER'):
-            raise ValueError("must be one of enum values ('ANTHROPIC', 'DEEPSEEK', 'FAL', 'GAMMA', 'GOOGLE', 'HEYGEN', 'JUSTAI', 'LINKHARBOR', 'MANUS', 'OPENAI', 'OPENROUTER', 'PARALLEL', 'PERPLEXITY', 'SBER', 'VEAI', 'YANDEX', 'OTHER')")
+        if value not in ('ANTHROPIC', 'DEEPSEEK', 'FAL', 'GAMMA', 'GOOGLE', 'HEYGEN', 'JUSTAI', 'LINKHARBOR', 'MANUS', 'OPENAI', 'OPENROUTER', 'PARALLEL', 'PERPLEXITY', 'SBER', 'SWOO', 'VEAI', 'YANDEX', 'OTHER'):
+            raise ValueError("must be one of enum values ('ANTHROPIC', 'DEEPSEEK', 'FAL', 'GAMMA', 'GOOGLE', 'HEYGEN', 'JUSTAI', 'LINKHARBOR', 'MANUS', 'OPENAI', 'OPENROUTER', 'PARALLEL', 'PERPLEXITY', 'SBER', 'SWOO', 'VEAI', 'YANDEX', 'OTHER')")
         return value
 
     @validator('currency')
